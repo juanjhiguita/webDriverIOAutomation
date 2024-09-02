f5e7577b2031bbbe39a9e7f5c2c1280a7026c1c8
Juan Jose Higuita Sanchez <juan.higuita@globant.com>
2024-09-02 16:05:15 -0500

    Refactor loginTest

--- src/test/java/com/automation/mobile/test/LoginTest.java
+++ src/test/java/com/automation/mobile/test/LoginTest.java
@@ -4,29 +4,42 @@ import com.automation.mobile.screens.HomeScreen;
 import com.automation.mobile.screens.LoginScreen;
 import com.automation.mobile.utils.tests.BaseTest;
 import org.openqa.selenium.bidi.log.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
+    private static final Logger log = LoggerFactory.getLogger(RegisterTest.class);
+    private LoginScreen loginScreen = new LoginScreen(driver);
+
+    @BeforeMethod
+    public void beforeTest() {
+        // PREVIOUS CONDITION: The user is in the login section and should have a user created previously
+        log.info("Test Start");
+        environmentSetup();
+        HomeScreen homeScreen = returnHomeScreen();
+        loginScreen = homeScreen.openLoginScreen();
+        Assert.assertTrue(loginScreen.verifyBeOnLoginScreen());
+    }
+
     @Test
     public void oneTest(){
-        // CONDICION PREVIA: El usuario debe estar en la sección de Login y tener un usuario previamente creado.
-
-        HomeScreen mainScreen = returnHomeScreen();
-
-        LoginScreen loginScreen = mainScreen.openLoginScreen();
-        // Assert para corroborar que se esta en la pagina de Login
-        Assert.assertTrue(loginScreen.verifyBeOnLoginScreen());
-        // Luego, verificar que el MENU "Login" es visible y clickeable, tambien mas propiedades
-        // Rellenar el formulario con la informacion del usuario previamente creado
+        // Set the credentials generated in the register test
         String email = LoginScreen.emailRegistered;
         String password = LoginScreen.passwordRegistered;
-
         loginScreen.fillLoginFormulary(email, password);
-        // Darle click al boton "login"
         Assert.assertTrue(loginScreen.isLoginBtnVisible());
         loginScreen.clickLoginBtn();
-        // Assert para comprobar el Alert
         Assert.assertTrue(loginScreen.isLoginAlertEqualsExpected());
+    }
+
+    @AfterTest
+    public void afterTest(){
+        driver.quit();
+        log.info("Test Finish");
     }
 }
